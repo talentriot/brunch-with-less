35d03fe6738758dd8a30f72c5620e032debd8805
Shawn Savageau <shawn.savageau@gmail.com>
2013-06-14 10:19:40 -0600

    changed remaining occurrences of 'home' into 'main' for continuity

--- test/views/main-layout-test.js
+++ test/views/main-layout-test.js
@@ -1,9 +1,9 @@
-describe('Home Page', function() {
-  var HomeLayout = require('views/main-layout');
-  var homeLayout = new HomeLayout();
+describe('Main Page', function() {
+  var MainLayout = require('views/main-layout');
+  var mainLayout = new MainLayout();
   var view, headerEl, layoutEl;
 
-  view = homeLayout.render().view;
+  view = mainLayout.render().view;
   view.trigger('route', 'index');
   beforeEach(function() {
     layoutEl = view.$el;
@@ -11,16 +11,16 @@ describe('Home Page', function() {
   });
   describe('Layout', function() {
     it('should have a jquery wrapped $el', function() {
-      expect(homeLayout).to.have.property('$el');
+      expect(mainLayout).to.have.property('$el');
     });
     it('layout should be rendered', function() {
-      homeLayout.__manager__.should.have.property('hasRendered')
+      mainLayout.__manager__.should.have.property('hasRendered')
         .that.equals(true);
     });
     it('$el should be able to find a child element', function() {
       expect(headerEl).to.have.length(1);
     });
-    it('there should be a link back to the homepage', function() {
+    it('there should be a link back to the mainpage', function() {
       expect(layoutEl.find('a[href="/"]')).to.have.length.of.at.least(1);
     });
     it('there should be content', function() {
