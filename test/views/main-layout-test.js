7a5d2d6a44801a2e70124d9cae43634e1af5a81a
Shawn Savageau <shawn.savageau@gmail.com>
2013-06-14 12:31:23 -0600

    'home' -> 'main' in test & required the route for view.trigger 2nd param

--- test/views/main-layout-test.js
+++ test/views/main-layout-test.js
@@ -1,20 +1,20 @@
-describe('Home Page', function() {
-  var HomeLayout = require('views/main-layout');
-  var homeLayout = new HomeLayout();
+describe('Main Page', function() {
+  var MainLayout = require('views/main-layout');
+  var mainLayout = new MainLayout();
   var view, headerEl, layoutEl;
 
-  view = homeLayout.render().view;
-  view.trigger('route', 'index');
+  view = mainLayout.render().view;
+  view.trigger('route', require('views/main/index'));
   beforeEach(function() {
     layoutEl = view.$el;
     headerEl = layoutEl.find('header');
   });
   describe('Layout', function() {
     it('should have a jquery wrapped $el', function() {
-      expect(homeLayout).to.have.property('$el');
+      expect(mainLayout).to.have.property('$el');
     });
     it('layout should be rendered', function() {
-      homeLayout.__manager__.should.have.property('hasRendered')
+      mainLayout.__manager__.should.have.property('hasRendered')
         .that.equals(true);
     });
     it('$el should be able to find a child element', function() {
